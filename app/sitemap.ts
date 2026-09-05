175b7cab9369a83f91d766267bfa371e55868ebd
diginoron <diginoron@gmail.com>
2026-09-05 15:21:55 +1000

    fix(seo): update sitemap with held courses and redirect /sitemap to /sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -40,6 +40,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${BASE_URL}/services/held-courses`,
+      lastModified: currentDate,
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
       url: `${BASE_URL}/courses`,
       lastModified: currentDate,
       changeFrequency: "weekly",
